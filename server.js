cebd213851aaf7e700473daf0ab4abed73d76cbb
RAZ <113300010+RAZZULLIX@users.noreply.github.com>
2025-01-31 00:54:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+/**
+ * server.js
+ *
+ * A minimal ephemeral chat server using Express + Socket.IO.
+ * Everyone in the same "channelUrl" sees each other's messages.
+ */
+
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
@@ -5,38 +12,36 @@ const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: "*"
-    }
+  cors: {
+    origin: "*"
+  }
 });
 
-// We'll just serve a small landing page so we know it's running
+// Serve a simple landing page (optional)
 app.get('/', (req, res) => {
-    res.send('urlings ephemeral chat server is running!');
+  res.send('urlings ephemeral chat server is running!');
 });
 
-// This is the ephemeral chat logic
+// Socket.IO ephemeral chat logic
 io.on('connection', (socket) => {
 
-    // Client tells us which channel (URL) to join
-    socket.on('joinChannel', (channelUrl) => {
-        socket.join(channelUrl);
-        console.log(`Socket ${socket.id} joined channel: ${channelUrl}`);
-    });
+  socket.on('joinChannel', (channelUrl) => {
+    socket.join(channelUrl);
+    console.log(`Socket ${socket.id} joined channel: ${channelUrl}`);
+  });
 
-    // When a client sends a "chatMessage", broadcast it to everyone in the same channel
-    socket.on('chatMessage', ({ channelUrl, text }) => {
-        // Send to everyone (including sender) in that channel
-        io.to(channelUrl).emit('chatMessage', { text });
-    });
+  socket.on('chatMessage', ({ channelUrl, text }) => {
+    // Broadcast to everyone (including sender) in that channel
+    io.to(channelUrl).emit('chatMessage', { text });
+  });
 
-    socket.on('disconnect', () => {
-        console.log(`Socket ${socket.id} disconnected`);
-    });
+  socket.on('disconnect', () => {
+    console.log(`Socket ${socket.id} disconnected`);
+  });
 });
 
-// Start server on the port that Render will provide, or 3000 locally
+// Start on port 3000 (local) or environment port
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
