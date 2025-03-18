87a88bb19afb0d4ee8d85660f651736a8ec09f77
RAZ <113300010+RAZZULLIX@users.noreply.github.com>
2025-03-18 12:22:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,19 @@ function getColorFromSocketId(socketId) {
 }
 
 function getDefaultUsername() {
-    const animals = ["tiger", "elephant", "monkey", "lion", "panda", "koala", "zebra", "giraffe"];
+    
+const animals = [
+  "tiger", "elephant", "monkey", "lion", "panda", "koala", "zebra", "giraffe",
+  // Famous animals
+  "bear", "wolf", "eagle", "shark", "whale", "otter", "rhino", "hyena", "cheetah", "octopus", "penguin", "koi", "seal", "beaver",
+  // Niche animals
+  "quokka", "axolotl", "okapi", "narwhal", "fennec", "meerkat", "tarsier", "capybara", "serval", "caracal", "binturong", "marmoset", "saiga", "dikdik", "takin", "quoll", "loris", "jerboa", "tenrec", "colugo", "pangolin", "aardvark", "fossa", "genet", "civet", "bongo", "nyala", "gerenuk", "addax", "kudu", "oryx", "gaur", "bison", "tapir", "saola", "pudu", "muntjac", "chamois", "markhor", "ibex", "tahr", "goral", "serow", "duiker", "sitatunga", "bontebok", "eland", "anoa", "banteng", "yak", "zebu", "warthog", "peccary", "babirusa",
+  // Exotic animals
+  "komodo", "anaconda", "chameleon", "peacock", "toucan", "macaw", "ocelot", "manatee", "dugong", "beluga", "orca", "dolphin", "porpoise", "bluewhale", "humpback", "jaguar", "leopard", "puma", "panther", "lynx", "margay", "manul", "sandcat", "kinkajou", "cacomistle", "palmcivet", "linsang", "tayra", "grison", "zorilla", "wallaby", "bilby", "numbat", "pademelon", "potoroo", "bandicoot", "echidna", "platypus", "wombat", "cuscus", "opossum", "kakapo", "kea", "tui", "kiwi", "budgerigar", "cockatoo", "corella", "galah", "rosella", "lorikeet", "parrot", "conure", "lovebird", "finch", "sparrow", "starling", "myna", "bulbul", "hornbill", "barbet", "beeeater", "kingfisher", "roller", "hoopoe", "jacamar", "motmot", "trogon", "quetzal", "oilbird", "potoo", "frogmouth", "nightjar", "owl", "condor", "shoebill", "flamingo", "stork", "ibis", "spoonbill", "heron", "egret", "bittern", "crane", "rail", "coot", "moorhen", "jacana", "avocet", "plover", "sandpiper", "snipe", "curlew", "godwit", "tattler", "turnstone", "phalarope", "skua", "gull", "tern", "skimmer", "auk", "puffin", "guillemot", "razorbill", "murre", "albatross", "petrel", "fulmar", "cormorant", "shag", "gannet", "booby", "darter", "anhinga", "grebe", "loon", "duck", "goose", "swan", "teal", "wigeon", "pintail", "shoveler", "merganser", "smew", "eider", "scoter", "goldeneye", "bufflehead", "canvasback", "redhead", "ruddyduck", "mallard", "gadwall", "garganey", "pochard", "tuftedduck", "scaup", "limpkin", "bustard", "kori", "rhea", "tinamou", "dodo", "moa",
+  // Funny animals
+  "blobfish", "proboscis", "armadillo", "hedgehog", "sloth", "puffin", "kiwi", "kakapo", "budgerigar", "bufflehead", "quetzal", "potoo", "frogmouth", "nightjar", "shoebill", "flamingo", "manatee", "dugong", "narwhal", "beluga", "orca", "bluewhale", "humpback", "jaguar", "ocelot", "margay", "kinkajou", "cacomistle", "palmcivet", "tayra", "zorilla", "bilby", "numbat"
+];
+    
     const animal = animals[Math.floor(Math.random() * animals.length)];
     const number = ("0" + Math.floor(Math.random() * 100)).slice(-2);
     return animal + number;
